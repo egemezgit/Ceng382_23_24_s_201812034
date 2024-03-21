332f86e63cf2bf774d92e81b407262c8597264f4
egemezgit <c1812034@student.cankaya.edu.tr>
2024-03-21 13:51:18 +0300

    New Branch was created from Lab4 branch, New Page was created, and Link was used on Index.csshtml

---
+++ WebApplication1/Pages/nPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class nPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
